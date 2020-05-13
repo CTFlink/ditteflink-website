e8fc6a74ce8980945c77da0001ae697d1c76b1e8
Christian Flink <ctflink@gmail.com>
2020-05-14 01:13:30 +0200

    Navbar

--- components/Nav.js
+++ components/Nav.js
@@ -2,8 +2,19 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="#">
-        Navbar
+        Ditte Flink Design
       </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
       <div className="collapse navbar-collapse" id="navbarColor01">
         <ul className="navbar-nav mr-auto">
@@ -14,7 +25,12 @@ const Nav = () => {
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">
-              Features
+              Webshop
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Dressmaker
             </a>
           </li>
           <li className="nav-item">
@@ -28,6 +44,16 @@ const Nav = () => {
             </a>
           </li>
         </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+          />
+          <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
